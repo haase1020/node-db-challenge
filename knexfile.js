b3e12a12ac72dd7f81a224a9d3c41f35d44c6524
Mandi Haase <haase1020@gmail.com>
2020-03-20 12:15:22 -0400

    test server and is working by sending message to postman

--- knexfile.js
+++ knexfile.js
@@ -5,10 +5,21 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/sprint-projects.db3'
     },
-    useNullAsDefault: true
-  },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+    }
+  }
+},
 
   staging: {
     client: 'postgresql',
