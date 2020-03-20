6bdf2628686655ccff3d871af5119fc207ca0bb0
Mandi Haase <haase1020@gmail.com>
2020-03-20 17:16:22 -0400

    MVP

--- projects/project-model.js
+++ projects/project-model.js
@@ -1,89 +1,118 @@
-const db = require('../data/db-config.js');
+const db = require('../data/db-config');
 
-module.exports = {
-    find,
-    findById,
-    findTasks,
-    findResources,
-    findProjectResources,
-    add,
-    addTask,
-    addResource,
-    addProjectResource,
-    update,
-    remove
+function addProject(project) {
+    return db('projects').insert(project);
+}
+function addResource(resource) {
+    return db('resources').insert(resource);
+}
+function addTask(task) {
+    return db('tasks').insert(task);
 }
 
-function find() {
+function findAllProjects() {
     return db('projects');
 }
-
-function findById(id) {
-    if(!db('projects').where({ id })){
-        return null;
-    } else {
-        return db('projects').where({ id }).first();
-    }
+function findAllResources() {
+    return db('resources');
+}
+function findAllTasks() {
+    return db('tasks');
 }
 
-function findTasks(id) {
-    return db('projects as p')
-    .where('p.id', id)
-    .join('tasks as t', 'p.id', 't.project_id')
-    .select('p.name', 'p.description', 't.task_number', 't.description', 't.notes')
-    .select('p.name', 'p.description', 't.task_number','t.description', 't.notes')
-    .where('t.project_id', id)
-    .orderBy('t.task_number');
-}
+module.exports = {
+    addProject,
+    addResource,
+    addTask,
+    findAllProjects,
+    findAllResources,
+    findAllTasks
+};
 
-function findResources() {
-    return db('resources as r')
-    .join('project_resources as pr', 'pr.resource_id', 'r.id')
-    .join('projects as p', 'p.id', 'pr.project_id')
-    .select('p.id as Project ID', 'p.name as Project', 
-    'r.id as Resource ID', 'r.name as Resource', 
-    'r.description as Description', 'pr.notes as Notes')
-    .where('p.id', id);
-}
+// module.exports = {
+//     find,
+//     findById,
+//     findTasks,
+//     findResources,
+//     findProjectResources,
+//     add,
+//     addTask,
+//     addResource,
+//     addProjectResource,
+//     update,
+//     remove
+// }
 
-function findProjectResources(id) {
-    return db('resources as r')
-    .join('project_resources as pr', 'pr.resource_id', 'r.id')
-    .join('projects as p', 'p.id', 'pr.project_id')
-    .select('p.name as Project Name', 'r.name as Resource Name', 'r.description as Description', 'pr.notes as Notes')
-    .where('p.id', id);
-}
+// function find() {
+//     return db('projects');
+// }
+
+// function findById(id) {
+//     if(!db('projects').where({ id })){
+//         return null;
+//     } else {
+//         return db('projects').where({ id }).first();
+//     }
+// }
+
+// function findTasks(id) {
+//     return db('projects as p')
+//     .where('p.id', id)
+//     .join('tasks as t', 'p.id', 't.project_id')
+//     .select('p.name', 'p.description', 't.task_number', 't.description', 't.notes')
+//     .select('p.name', 'p.description', 't.task_number','t.description', 't.notes')
+//     .where('t.project_id', id)
+//     .orderBy('t.task_number');
+// }
+
+// function findResources() {
+//     return db('resources as r')
+//     .join('project_resources as pr', 'pr.resource_id', 'r.id')
+//     .join('projects as p', 'p.id', 'pr.project_id')
+//     .select('p.id as Project ID', 'p.name as Project', 
+//     'r.id as Resource ID', 'r.name as Resource', 
+//     'r.description as Description', 'pr.notes as Notes')
+    
+// }
+
+// function findProjectResources(id) {
+//     return db('resources as r')
+//     .join('project_resources as pr', 'pr.resource_id', 'r.id')
+//     .join('projects as p', 'p.id', 'pr.project_id')
+//     .select('p.name as Project Name', 'r.name as Resource Name', 'r.description as Description', 'pr.notes as Notes')
+//     .where('p.id', id);
+// }
 
 
-function add(project) {
-    return db('projects')
-    .insert(project, 'id');
-}
+// function add(project) {
+//     return db('projects')
+//     .insert(project, 'id');
+// }
 
-function addTask(task) {
-    return db('tasks')
-    .insert(task,'id');
-}
+// function addTask(task) {
+//     return db('tasks')
+//     .insert(task,'id');
+// }
 
-function addResource(resource) {
-    return db('resources')
-    .insert(resource);
-}
+// function addResource(resource) {
+//     return db('resources')
+//     .insert(resource);
+// }
 
-function addProjectResource(id, project_resource) {
-    return db('project_resources')
-    .where('project_id', id)
-    .insert(project_resource);
-}
+// function addProjectResource(id, project_resource) {
+//     return db('project_resources')
+//     .where('project_id', id)
+//     .insert(project_resource);
+// }
 
-function update(id,changes) {
-    return db('projects')
-    .where({ id })
-    .update(changes);
-}
+// function update(id,changes) {
+//     return db('projects')
+//     .where({ id })
+//     .update(changes);
+// }
 
-function remove(id) {
-    return db('projects')
-    .where({ id })
-    .del();
-}
+// function remove(id) {
+//     return db('projects')
+//     .where({ id })
+//     .del();
+// }
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
-
-const ProjectRouter = require('./projects/project-router.js');
-const ResourceRouter = require('./projects/resource-router.js');
-
+const helmet = require('helmet')
+const morgan = require('morgan')
+const cors = require('cors')
 const server = express();
+const projectRouter = require('./projects/project-router')
 
-server.use(express.json());
-server.use('/api/projects', ProjectRouter);
-server.use('/api/resources', ResourceRouter);
+server.use(helmet())
+server.use(express.json())
+server.use(morgan('dev'))
+server.use(cors())
+server.use('/api/projects', projectRouter);
+
+
 
 server.get('/', (req,res) => {
     res.send ('<h2> Welcome To The Land of Projects</h2>');
