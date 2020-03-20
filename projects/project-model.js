4308ec1a46a578d683a93d0ecdc394da8b0f1a6f
Mandi Haase <haase1020@gmail.com>
2020-03-20 15:06:10 -0400

    update project model file to complete functions

--- projects/project-model.js
+++ projects/project-model.js
@@ -4,14 +4,14 @@ module.exports = {
     find,
     findById,
     findTasks,
-    // findResources,
-    // findProjectResources,
-    // add,
-    // addTask,
-    // addResource,
-    // addProjectResource,
-    // update,
-    // remove
+    findResources,
+    findProjectResources,
+    add,
+    addTask,
+    addResource,
+    addProjectResource,
+    update,
+    remove
 }
 
 function find() {
@@ -34,4 +34,56 @@ function findTasks(id) {
     .select('p.name', 'p.description', 't.task_number','t.description', 't.notes')
     .where('t.project_id', id)
     .orderBy('t.task_number');
+}
+
+function findResources() {
+    return db('resources as r')
+    .join('project_resources as pr', 'pr.resource_id', 'r.id')
+    .join('projects as p', 'p.id', 'pr.project_id')
+    .select('p.id as Project ID', 'p.name as Project', 
+    'r.id as Resource ID', 'r.name as Resource', 
+    'r.description as Description', 'pr.notes as Notes')
+    .where('p.id', id);
+}
+
+function findProjectResources(id) {
+    return db('resources as r')
+    .join('project_resources as pr', 'pr.resource_id', 'r.id')
+    .join('projects as p', 'p.id', 'pr.project_id')
+    .select('p.name as Project Name', 'r.name as Resource Name', 'r.description as Description', 'pr.notes as Notes')
+    .where('p.id', id);
+}
+
+
+function add(project) {
+    return db('projects')
+    .insert(project, 'id');
+}
+
+function addTask(task) {
+    return dbv('tasks')
+    .insert(task,'id');
+}
+
+function addResource(resource) {
+    return db('resources')
+    .insert(resource);
+}
+
+function addProjectResource(id, project_resource) {
+    return db('project_resources')
+    .where('project_id', id)
+    .insert(project_resource);
+}
+
+function update(id,changes) {
+    return db('projects')
+    .where({ id })
+    .update(changes);
+}
+
+function remove(id) {
+    return db('projects')
+    .where({ id })
+    .del();
 }
